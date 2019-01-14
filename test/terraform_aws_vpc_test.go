149408c91f0a64ac15690b84f77b306e6054c96b
Joel Castillo <jocastillo@records.nyc.gov>
2019-01-14 12:04:36 -0500

    Starting from scratch

---
+++ test/terraform_aws_vpc_test.go
@@ -0,0 +1,9 @@
+package test
+
+import {
+	"fmt"
+	"strings"
+	"testing"
+	"github.com/stretchr/testify/assert"
+}
+
--- tests/provider.tf
+++
@@ -1,8 +0,0 @@
-variable "aws_shared_credentials_file" {}
-variable "aws_profile" {}
-
-provider "aws" {
-  shared_credentials_file = "${var.aws_shared_credentials_file}"
-  profile                 = "${var.aws_profile}"
-  region                  = "${var.aws_region}"
-}
